ad64adeeeebd965c711e88fb1e556bfd02ca48d4
khutso-student <khutsomakunyane1@gmail.com>
2025-10-31 12:22:39 +0200

    New Loading UI added

--- src/component/Loading.jsx
+++ src/component/Loading.jsx
@@ -1,13 +1,15 @@
+import { Atom } from 'react-loading-indicators';
 import { motion } from "framer-motion";
 
 export default function Loading() {
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-[#000] z-50">
-      <motion.div
+      {/* <motion.div
         className="w-16 h-16 border-4 border-t-[#11DAD1] border-white rounded-full"
         animate={{ rotate: 360 }}
         transition={{ repeat: Infinity, duration: 1 }}
-      />
+      /> */}
+      <Atom color="#31ccbc" size="large" text="" textColor="" />
     </div>
   );
 }
