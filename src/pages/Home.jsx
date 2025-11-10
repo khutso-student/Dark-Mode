ec7e9b726389b933f7f26c31833554c9f7fa1e3a
khutso-student <khutsomakunyane1@gmail.com>
2025-11-10 11:07:54 +0200

    New updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -168,8 +168,8 @@ export default function Main() {
         <Projects />
       </div>
 
-      <div className="flex justify-center items-center w-full h-auto  p-2 sm:p-5 mt-5">
-            <div className="flex flex-col sm:flex-row justify-center items-center gap-4 w-full sm:w-[90%] h-150 bg-[#0E0E10] border border-[#868686] rounded-xl p-2 sm:p-3">
+      <div className="flex justify-center items-center w-full h-auto   p-2 sm:p-5 mt-5">
+            <div className="flex flex-col sm:flex-row justify-center items-center gap-4 w-full  h-150 bg-[#0E0E10] border border-[#868686] rounded-xl p-2 ">
                 <div className="flex justify-center items-center w-full h-full border rounded-xl border-[#868686]"
                 style={{ backgroundImage: `url(${ArtImage})` }}>
                     <WorkCarousel />
